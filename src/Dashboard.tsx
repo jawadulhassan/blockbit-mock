8dadec8dd0d341d4e2e6a7cce422f06315f1bb10
jawadulhassan <jawadulhassan50@gmail.com>
2022-04-27 03:14:36 +0500

    auth backtesting test

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,12 +1,15 @@
 import React, { FC, useState } from 'react';
 
 import Dashboard from 'containers/Dashboard';
-import ErrorBoundary from 'components/widgets/ErrorBoundary';
+
+import { AuthHeader } from 'components/views/Header';
+import ErrorBoundary from 'components/widgets/ErrorBoundary'; 
 
 const DashboardHandler: FC<any> = (): any => {
   const [selectedTab, setSelectedTab] = useState('backtesting'); 
   return (
     <ErrorBoundary>
+      <AuthHeader selectedTab={selectedTab} />
       <Dashboard  selectedTab={selectedTab} setSelectedTab={setSelectedTab}/>
     </ErrorBoundary>
   );
--- src/components/views/Dashboard/BackTesting/Home/index.tsx
+++ src/components/views/Dashboard/BackTesting/Home/index.tsx
@@ -1,16 +1,10 @@
-import React, { useState, FC, useContext, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, FC } from 'react';
 
-import { apiClient } from 'shared/services/api';
 import { shapeDataFormatter } from 'shared/helpers';
-import { ROUTE_CONSTANTS } from 'shared/constants/Routes';
-import StorageConstants from 'shared/constants/StorageConstants';
-import FirebaseAuthContext from 'shared/contexts/firebaseContext';
 import {
-  getTestQuotaInfoEndpoint,
-  getBackTestHistoryEndpoint,
-  getIndividualTestResultEndpoint,
-} from 'shared/endPoints';
+  mockBacktesting,
+  mockIndividualTests,
+} from 'shared/mockData/backtestHistory';
 import {
   FlexRow,
   FlexColumn,
@@ -38,20 +32,15 @@ const BackTesting: FC<any> = ({
   setUserQuota,
   setSelectedStep,
 }: any): any => {
-
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading] = useState(false);
   const [displayResult, setDisplayResult] = useState('');
   const [selectedFilter, setSelectedFilter] = useState({
     filter: '',
     value: '',
   });
-  const [backtestHistory, setBacktestHistory] = useState([]);
+  const [backtestHistory] = useState(mockBacktesting);
 
-
-
-  const handleSearch = () => {
-
-  };
+  const handleSearch = () => {};
 
   return (
     <ScreenWrapper>
@@ -145,33 +134,11 @@ const IndividualTest: FC<any> = ({
     (item: any): any => item.resultID === resultId
   );
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [individualData, setIndividualData] = useState({
-    netProfitLoss: 0,
-    trades: [],
+  const [isLoading] = useState(false);
+  const [individualData] = useState({
+    netProfitLoss: 4,
+    trades: mockIndividualTests,
   });
-
-  useEffect((): void => {
-    setIsLoading(true);
-    getIndividualTestData(resultId);
-  }, [resultId]);
-
-  async function getIndividualTestData(id: any): Promise<any> {
-    apiClient('')
-      .get(`${getIndividualTestResultEndpoint}?id=${id}`)
-      .then((response: any): any => {
-        const data = response?.data?.data?.record;
-        setIndividualData(data);
-        setIsLoading(false);
-      })
-      .catch((err: any): any => {
-        if (err.response.status === 401) {
-          setIsLoading(false);
-          localStorage.setItem(StorageConstants.AUTH_TOKEN, '');
-          localStorage.setItem(StorageConstants.USER_DATA, '');
-        }
-      });
-  }
 
   const plotData = shapeDataFormatter(individualData.trades);
   return (
