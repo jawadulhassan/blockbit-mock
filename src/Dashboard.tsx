69b9ddba42429b483981563ff7875c5e777b5d30
jawadulhassan <jawadulhassan50@gmail.com>
2022-05-20 22:23:31 +0500

    Not mobile (desktop or laptop or tablet)

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,16 +1,33 @@
 import React, { FC, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 import Dashboard from 'containers/Dashboard';
 
 import { AuthHeader } from 'components/views/Header';
 import ErrorBoundary from 'components/widgets/ErrorBoundary';
 
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return isMobile ? children : null;
+};
+
+const Default = ({ children }) => {
+  const isNotMobile = useMediaQuery({ minWidth: 768 });
+  return isNotMobile ? children : null;
+};
+
 const DashboardHandler: FC<any> = (): any => {
   const [selectedTab, setSelectedTab] = useState('dashboard');
+
   return (
     <ErrorBoundary>
-      <AuthHeader selectedTab={selectedTab} />
-      <Dashboard selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      <Mobile>
+        <div className="is-error">Not mobile (desktop or laptop or tablet)</div>
+      </Mobile>
+      <Default>
+        <AuthHeader selectedTab={selectedTab} />
+        <Dashboard selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      </Default>
     </ErrorBoundary>
   );
 };
